b4f9e85765109c58ce6e2c7b078928d574f78733
Bradlee King <kingofbrad@gmail.com>
2024-02-15 17:05:55 +0000

    Initial Commit

---
+++ Orokin Collective/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orokin Collective
+//
+//  Created by Bradlee King on 15/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orokin Collective/Orokin_CollectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orokin_CollectiveApp.swift
+//  Orokin Collective
+//
+//  Created by Bradlee King on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Orokin_CollectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
